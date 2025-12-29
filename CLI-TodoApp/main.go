91c9d562a9b6dd5949f93ac48ce0228fafea2e45
niteshkumar <niteshkumar@niteshkumars-MacBook-Pro.local>
2025-12-29 11:53:46 +0530

    CLI todo app

--- CLI-TodoApp/main.go
+++ CLI-TodoApp/main.go
@@ -1,12 +1,19 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
 
 func main(){
 	todos := Todos{}
 	storage := NewStorage[Todos]("todos.json")
 	storage.Load(&todos)
-	cmdFlags := CmdFlags{}
-	cmdFlags.Execute(&todos)
+	cmdFlags := NewCommandFlags()
+	if err := cmdFlags.Execute(&todos); err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
 	// todos.Add("Buy Milk")
 	// todos.Add("Buy Bread")
 	// todos.Toggle(0)
