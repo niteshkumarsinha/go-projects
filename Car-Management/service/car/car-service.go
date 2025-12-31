079d1841a74716de33791e9d9dfbd177532ac606
niteshkumar <niteshkumar@niteshkumars-MacBook-Pro.local>
2025-12-31 11:52:24 +0530

    car service

--- Car-Management/service/car/car-service.go
+++ Car-Management/service/car/car-service.go
@@ -1,1 +1,66 @@
 package service
+
+import (
+	"context"
+
+	"github.com/nitesh111sinha/car-management/models"
+	"github.com/nitesh111sinha/car-management/store"
+)
+
+type CarService struct {
+	store store.CarStoreInterface
+}
+
+func NewCarService(store store.CarStoreInterface) *CarService {
+	return &CarService{
+		store: store,
+	}
+}
+
+func (s *CarService) GetCarById(ctx context.Context, carID string) (models.Car, error) {
+	car, err := s.store.GetCarById(ctx, carID)
+	if err != nil {
+		return models.Car{}, err
+	}
+	return car, nil
+}
+
+func (s *CarService) GetCars(ctx context.Context) ([]models.Car, error) {
+	cars, err := s.store.GetCars(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return cars, nil
+}
+
+func (s *CarService) UpdateCar(ctx context.Context, car models.Car) (models.Car, error) {
+	updatedCar, err := s.store.UpdateCar(ctx, car)
+	if err != nil {
+		return models.Car{}, err
+	}
+	return updatedCar, nil
+}
+
+func (s *CarService) DeleteCar(ctx context.Context, carID string) error {
+	if err := s.store.DeleteCar(ctx, carID); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s *CarService) GetCarByBrand(ctx context.Context, brand string, isEngine bool) ([]models.Car, error) {
+	cars, err := s.store.GetCarByBrand(ctx, brand, isEngine)
+	if err != nil {
+		return nil, err
+	}
+	return cars, nil
+}
+
+func (s *CarService) CreateCar(ctx context.Context, car models.Car) (models.Car, error) {
+	createdCar, err := s.store.CreateCar(ctx, car)
+	if err != nil {
+		return models.Car{}, err
+	}
+	return createdCar, nil
+}
+
--- Car-Management/store/interface.go
+++ Car-Management/store/interface.go
@@ -1,2 +1,23 @@
 package store
 
+import (
+	"context"
+	"github.com/nitesh111sinha/car-management/models"
+)
+
+type CarStoreInterface interface {
+	CreateCar(ctx context.Context, car models.Car) (models.Car, error)
+	GetCarById(ctx context.Context, carID string) (models.Car, error)
+	GetCars(ctx context.Context) ([]models.Car, error)
+	UpdateCar(ctx context.Context, car models.Car) (models.Car, error)
+	DeleteCar(ctx context.Context, carID string) error
+	GetCarByBrand(ctx context.Context, brand string, isEngine bool) ([]models.Car, error)
+}
+
+type EngineStoreInterface interface {
+	CreateEngine(ctx context.Context, engine models.Engine) (models.Engine, error)
+	GetEngineById(ctx context.Context, engineID string) (models.Engine, error)
+	GetEngines(ctx context.Context) ([]models.Engine, error)
+	UpdateEngine(ctx context.Context, engine models.Engine) (models.Engine, error)
+	DeleteEngine(ctx context.Context, engineID string) error
+}	
